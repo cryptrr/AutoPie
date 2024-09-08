907027386b3bd97043e4d2aa18cf1c40cbb29936
amal <amalshaji645@gmail.com>
2024-09-08 21:38:54 +0530

    Bumped version to 0.9.1-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 27
         //noinspection EditedTargetSdkVersion,ExpiredTargetSdkVersion
         targetSdk = 28
-        versionCode = 3
-        versionName = "\"0.8-beta\""
+        versionCode = 5
+        versionName = "\"0.9.1-beta\""
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
