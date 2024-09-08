35b9a4258678732dfcb8d5e8e3c5173b8225bc9d
amal <amalshaji645@gmail.com>
2024-09-08 14:21:12 +0530

    Updated INIT_ARCHIVE_URL

--- app/src/main/java/com/autosec/pie/data/Constants.kt
+++ app/src/main/java/com/autosec/pie/data/Constants.kt
@@ -2,7 +2,7 @@ package com.autosec.pie.data
 
 class AutoPieConstants {
     companion object {
-        const val AUTOPIE_INIT_ARCHIVE_URL = "https://github.com/cryptrr/AutoPie/releases/download/v0.08-beta/autopie-init-0.02.tar.xz"
+        const val AUTOPIE_INIT_ARCHIVE_URL = "https://github.com/cryptrr/AutoPie/releases/download/v0.09-beta/autopie-init-0.03.tar.xz"
         const val AUTOPIE_SHELL_RESULT_REGEX = "<AUTOPIE_RESULT>(.*?)<AUTOPIE_RESULT/>"
     }
 }
