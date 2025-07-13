b559d6f89b89008b5c39a3f7290fed653cb792d2
amal <amalshaji645@gmail.com>
2025-07-13 08:16:33 +0530

    Bumped version to v0.14.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 27
         //noinspection EditedTargetSdkVersion,ExpiredTargetSdkVersion
         targetSdk = 28
-        versionCode = 18
-        versionName = "\"0.13.3-beta\""
+        versionCode = 19
+        versionName = "\"0.14.0-beta\""
 
         testInstrumentationRunner = "com.autosec.pie.AutoPieTestRunner"
         vectorDrawables {
