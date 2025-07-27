7c3dbe08cf8a0222f303141c4e97a90d0aae3ea4
amal <amalshaji645@gmail.com>
2025-07-28 00:05:48 +0530

    Upgraded to AGP 8.11.1 and Compose 07/2025

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,11 +56,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "20"
     }
     buildFeatures {
         compose = true
@@ -82,15 +82,15 @@ android {
 dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
-    implementation("androidx.compose.material:material-icons-extended:1.7.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("com.google.code.gson:gson:2.11.0")
-    implementation("org.apache.commons:commons-compress:1.21")
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.navigation:navigation-compose:2.9.2")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.07.00"))
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation("org.apache.commons:commons-compress:1.27.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation("org.tukaani:xz:1.9")
     implementation("com.jaredrummler:ktsh:1.0.0")
     implementation("androidx.compose.ui:ui")
