6a313594d41cd27ac72520a4ae8018192739f900
amal <amalshaji645@gmail.com>
2025-08-05 01:49:05 +0530

    Reverted to Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,11 +63,11 @@ android {
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
 
-        sourceCompatibility = JavaVersion.VERSION_20
-        targetCompatibility = JavaVersion.VERSION_20
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "20"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
