4cb4f5dca6caacc0d252e6af6a1a37c983262a2f
amal <amalshaji645@gmail.com>
2025-08-06 23:20:08 +0530

    Added error handling in pinAppShortcut()

--- app/src/main/java/com/autosec/pie/autopieapp/presentation/screens/CommandDetailsSheet.kt
+++ app/src/main/java/com/autosec/pie/autopieapp/presentation/screens/CommandDetailsSheet.kt
@@ -29,9 +29,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.autosec.pie.BuildConfig
 import com.autosec.pie.DirectCommandActivity
-import com.autosec.pie.OutputViewerActivity
 import com.autosec.pie.R
 import com.autosec.pie.autopieapp.data.ShareInputs
 import com.autosec.pie.autopieapp.domain.ViewModelEvent
@@ -42,6 +40,7 @@ import com.autosec.pie.autopieapp.presentation.viewModels.CreateCommandViewModel
 import com.autosec.pie.autopieapp.presentation.viewModels.ShareReceiverViewModel
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
+import timber.log.Timber
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -65,33 +64,38 @@ fun CommandDetailsSheet(
     val context = LocalContext.current
 
     fun pinAppShortcut(context: Context, commandId: String, shortLabel: String, longLabel: String) {
-        val shortcutManager = context.getSystemService(ShortcutManager::class.java)
+        try {
+            val shortcutManager = context.getSystemService(ShortcutManager::class.java)
 
-        if (shortcutManager.isRequestPinShortcutSupported) {
-            val shortcut = ShortcutInfo.Builder(context, commandId)
-                .setShortLabel(shortLabel)
-                .setLongLabel(longLabel)
-                .setIcon(Icon.createWithResource(context, R.mipmap.ic_launcher))
-                .setIntent(
-                    Intent(Intent.ACTION_MAIN).apply {
-                        setClass(context, DirectCommandActivity::class.java)
-                        putExtra("commandId", commandId)
-                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    }
+            if (shortcutManager.isRequestPinShortcutSupported) {
+                val shortcut = ShortcutInfo.Builder(context, commandId)
+                    .setShortLabel(shortLabel)
+                    .setLongLabel(longLabel)
+                    .setIcon(Icon.createWithResource(context, R.mipmap.ic_launcher))
+                    .setIntent(
+                        Intent(Intent.ACTION_MAIN).apply {
+                            setClass(context, DirectCommandActivity::class.java)
+                            putExtra("commandId", commandId)
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                    )
+                    .build()
+
+                val pinnedShortcutCallbackIntent =
+                    shortcutManager.createShortcutResultIntent(shortcut)
+                val successCallback = PendingIntent.getBroadcast(
+                    context, 0, pinnedShortcutCallbackIntent, PendingIntent.FLAG_IMMUTABLE
                 )
-                .build()
 
-            val pinnedShortcutCallbackIntent = shortcutManager.createShortcutResultIntent(shortcut)
-            val successCallback = PendingIntent.getBroadcast(
-                context, 0, pinnedShortcutCallbackIntent, PendingIntent.FLAG_IMMUTABLE
-            )
-
-            shortcutManager.requestPinShortcut(shortcut, successCallback.intentSender)
+                shortcutManager.requestPinShortcut(shortcut, successCallback.intentSender)
+            }
+        } catch (e: Exception) {
+            Timber.e(e)
         }
     }
 
 
-    val optionsList = remember{
+    val optionsList = remember {
         listOf(
             OptionItem(
                 text = "EDIT",
@@ -123,7 +127,11 @@ fun CommandDetailsSheet(
                 onClick = {
                     scope.launch {
                         open.value = false
-                        shareReceiverViewModel.main.dispatchEvent(ViewModelEvent.OpenCommandHistory(card))
+                        shareReceiverViewModel.main.dispatchEvent(
+                            ViewModelEvent.OpenCommandHistory(
+                                card
+                            )
+                        )
                     }
                 }
             ),
@@ -138,7 +146,7 @@ fun CommandDetailsSheet(
                 }
             ),
 
-        )
+            )
     }
 
 
@@ -155,31 +163,31 @@ fun CommandDetailsSheet(
         )
         {
 
-           Column(Modifier.padding(15.dp)){
+            Column(Modifier.padding(15.dp)) {
 
-               Spacer(modifier = Modifier.height(10.dp))
+                Spacer(modifier = Modifier.height(10.dp))
 
-               Text(
-                   text = card.name,
-                   lineHeight = 32.sp,
-                   fontSize = 28.sp,
-                   fontWeight = FontWeight.Bold,
-                   color = MaterialTheme.colorScheme.onPrimaryContainer
-               )
+                Text(
+                    text = card.name,
+                    lineHeight = 32.sp,
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
 
-               Spacer(modifier = Modifier.height(20.dp))
+                Spacer(modifier = Modifier.height(20.dp))
 
-               OptionLayout(Modifier, optionList = optionsList)
+                OptionLayout(Modifier, optionList = optionsList)
 
-               Spacer(Modifier.height(7.dp))
+                Spacer(Modifier.height(7.dp))
 
-               AutoPiePrimaryButton("RUN") {
-                   shareReceiverViewModel.currentExtrasDetails.value =
-                       Triple(true, card, ShareInputs())
-                   open.value = false
-               }
+                AutoPiePrimaryButton("RUN") {
+                    shareReceiverViewModel.currentExtrasDetails.value =
+                        Triple(true, card, ShareInputs())
+                    open.value = false
+                }
 
-           }
+            }
 
         }
 
