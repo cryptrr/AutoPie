2c9976a39704b58ebe76c7b1b6bc9705b37fc350
amal <amalshaji645@gmail.com>
2025-08-11 01:32:54 +0530

    Changed directory structure of AutoPie to reflect termux.

--- app/src/main/assets/env.sh
+++ app/src/main/assets/env.sh
@@ -5,16 +5,14 @@ echo "FILES DIR : $CUSTOM_DIR"
 
 export ANDROID_PACKAGE_NAME=$PACKAGE_NAME
 
-export PATH=$CUSTOM_DIR/build/usr/bin:$CUSTOM_DIR/build/bin:$PATH
+export PATH=$CUSTOM_DIR/usr/bin:$CUSTOM_DIR/bin:$PATH
 
-if [ ! -z "$LD_LIBRARY_PATH" ] ; then
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:"
-fi
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH$CUSTOM_DIR/build/usr/lib"
-export SSL_CERT_FILE="$CUSTOM_DIR/build/etc/ssl/cert.pem"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}$CUSTOM_DIR/usr/lib"
+
+export SSL_CERT_FILE="$CUSTOM_DIR/etc/ssl/cert.pem"
 # For ncurses
-export TERMINFO="$CUSTOM_DIR/build/usr/share/terminfo"
+export TERMINFO="$CUSTOM_DIR/usr/share/terminfo"
 
 export HOME="$CUSTOM_DIR"
-export PYTHONHOME="$CUSTOM_DIR/build"
-export PYTHONPATH="$CUSTOM_DIR/build/usr/lib/python3.12:$CUSTOM_DIR/build/usr/lib/python3.12/site-packages:$CUSTOM_DIR/build/usr/lib/python3.12/lib-dynload"
+export PYTHONHOME="$CUSTOM_DIR"
+export PYTHONPATH="$CUSTOM_DIR/usr/lib/python3.12:$CUSTOM_DIR/usr/lib/python3.12/site-packages:$CUSTOM_DIR/usr/lib/python3.12/lib-dynload"
