5503320aeafb24fe916a5a332f0f0c6c754f2cf8
amal <amalshaji645@gmail.com>
2025-08-15 07:35:42 +0530

    Turned off Content providers for Packages and Commands.

--- app/src/main/java/com/autosec/pie/Provider.kt
+++ app/src/main/java/com/autosec/pie/Provider.kt
@@ -14,60 +14,60 @@ import timber.log.Timber
 import java.io.File
 import kotlin.getValue
 
-class PackagesProvider : ContentProvider() {
-
-    val useCases: AutoPieUseCases by inject(AutoPieUseCases::class.java)
-
-    override fun query(
-        uri: Uri,
-        projection: Array<String>?,
-        selection: String?,
-        selectionArgs: Array<String>?,
-        sortOrder: String?
-    ): Cursor? {
-
-        if(context == null){
-            return null
-        }
-
-        val apps = useCases.getInstalledPackages(context!!.filesDir)
-
-        val matrix = MatrixCursor(arrayOf("packageName"))
-        apps.forEach { matrix.addRow(arrayOf(it)) }
-        return matrix
-    }
-    override fun onCreate() = true
-    override fun getType(uri: Uri) = "vnd.android.cursor.dir/vnd.autosec.pie"
-    override fun insert(uri: Uri, values: ContentValues?) = null
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?) = 0
-    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?) = 0
-}
-
-class CommandsProvider : ContentProvider() {
-
-    val useCases: AutoPieUseCases by inject(AutoPieUseCases::class.java)
-
-    override fun query(
-        uri: Uri,
-        projection: Array<String>?,
-        selection: String?,
-        selectionArgs: Array<String>?,
-        sortOrder: String?
-    ): Cursor? {
-
-        val commands = useCases.getCommandsList()
-
-        val matrix = MatrixCursor(arrayOf("command"))
-        commands.forEach { matrix.addRow(arrayOf(it.name)) }
-
-        return matrix
-    }
-    override fun onCreate() = true
-    override fun getType(uri: Uri) = "vnd.android.cursor.dir/vnd.autosec.pie"
-    override fun insert(uri: Uri, values: ContentValues?) = null
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?) = 0
-    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?) = 0
-}
+//class PackagesProvider : ContentProvider() {
+//
+//    val useCases: AutoPieUseCases by inject(AutoPieUseCases::class.java)
+//
+//    override fun query(
+//        uri: Uri,
+//        projection: Array<String>?,
+//        selection: String?,
+//        selectionArgs: Array<String>?,
+//        sortOrder: String?
+//    ): Cursor? {
+//
+//        if(context == null){
+//            return null
+//        }
+//
+//        val apps = useCases.getInstalledPackages(context!!.filesDir)
+//
+//        val matrix = MatrixCursor(arrayOf("packageName"))
+//        apps.forEach { matrix.addRow(arrayOf(it)) }
+//        return matrix
+//    }
+//    override fun onCreate() = true
+//    override fun getType(uri: Uri) = "vnd.android.cursor.dir/vnd.autosec.pie"
+//    override fun insert(uri: Uri, values: ContentValues?) = null
+//    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?) = 0
+//    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?) = 0
+//}
+//
+//class CommandsProvider : ContentProvider() {
+//
+//    val useCases: AutoPieUseCases by inject(AutoPieUseCases::class.java)
+//
+//    override fun query(
+//        uri: Uri,
+//        projection: Array<String>?,
+//        selection: String?,
+//        selectionArgs: Array<String>?,
+//        sortOrder: String?
+//    ): Cursor? {
+//
+//        val commands = useCases.getCommandsList()
+//
+//        val matrix = MatrixCursor(arrayOf("command"))
+//        commands.forEach { matrix.addRow(arrayOf(it.name)) }
+//
+//        return matrix
+//    }
+//    override fun onCreate() = true
+//    override fun getType(uri: Uri) = "vnd.android.cursor.dir/vnd.autosec.pie"
+//    override fun insert(uri: Uri, values: ContentValues?) = null
+//    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?) = 0
+//    override fun update(uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<String>?) = 0
+//}
 
 class ProcessStatusProvider : ContentProvider() {
 
