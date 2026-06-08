e9b90dba35957fd7a384de208b02f88790425dae
amal <amalshaji645@gmail.com>
2026-06-09 01:25:12 +0530

    Late commit

--- app/src/main/java/com/autosec/pie/use_case/RunCommandForUrl.kt
+++ app/src/main/java/com/autosec/pie/use_case/RunCommandForUrl.kt
@@ -19,8 +19,14 @@ import java.net.URL
 import kotlin.io.path.Path
 import kotlin.io.path.absolutePathString
 
-class RunCommandForUrl(private val processManagerService: ProcessManagerService){
-    suspend operator fun invoke(item: CommandModel, currentLink: String, fileUris: List<String>, commandExtraInputs: List<CommandExtraInput> = emptyList(), processId: Int) : Flow<CommandResult> {
+class RunCommandForUrl(private val processManagerService: ProcessManagerService) {
+    suspend operator fun invoke(
+        item: CommandModel,
+        currentLink: String,
+        fileUris: List<String>,
+        commandExtraInputs: List<CommandExtraInput> = emptyList(),
+        processId: Int
+    ): Flow<CommandResult> {
 
         return flow {
             Timber.d("runCommandForUrl")
@@ -32,25 +38,30 @@ class RunCommandForUrl(private val processManagerService: ProcessManagerService)
 
             val filename = inputUrl.file
 
-            val path = Path(Environment.getExternalStorageDirectory().absolutePath, item.path).absolutePathString()
+            val path = Path(
+                Environment.getExternalStorageDirectory().absolutePath,
+                item.path
+            ).absolutePathString()
 
             val execFilePath =
                 Environment.getExternalStorageDirectory().absolutePath + "/AutoSec/bin/" + item.exec
 
-            val (execType,fullExecPath, resultCommand) = when{
+            val (execType, fullExecPath, resultCommand) = when {
                 File(item.exec).isAbsolute -> {
                     Timber.d("Using package absolute path")
-                   ExecAndCommand( ExecType.ABSOLUTE_PATH,item.exec,"\"${item.command}\"")
+                    ExecAndCommand(ExecType.ABSOLUTE_PATH, item.exec, "\"${item.command}\"")
                 }
+
                 File(execFilePath).exists() -> {
                     Timber.d("Using autopie package")
                     //For packages installed inside autosec/bin
-                    ExecAndCommand( ExecType.AUTOPIE_PACKAGE,execFilePath,"\"${item.command}\"")
+                    ExecAndCommand(ExecType.AUTOPIE_PACKAGE, execFilePath, "\"${item.command}\"")
                 }
+
                 else -> {
                     //Base case fallback to terminals installed packages such as busybox packages.
                     Timber.d("Using shell installed program")
-                    ExecAndCommand( ExecType.SHELL_INSTALLED,item.exec, item.command)
+                    ExecAndCommand(ExecType.SHELL_INSTALLED, item.exec, item.command)
                 }
             }
 
@@ -60,18 +71,50 @@ class RunCommandForUrl(private val processManagerService: ProcessManagerService)
 
 
             val inputParsedData = mutableListOf<InputParsedData>().also {
-                it.add(InputParsedData(name = "INPUT_FILE", value = if(useQuotes) "\"$currentLink\"" else currentLink))
-                it.add(InputParsedData(name = "INPUT_URL", value = if(useQuotes) "\"$currentLink\"" else currentLink))
-                it.add(InputParsedData(name = "HOST", value = if(useQuotes) "\"$host\"" else host))
-                it.add(InputParsedData(name = "FILENAME", value = if(useQuotes) "\"$filename\"" else filename))
+                it.add(
+                    InputParsedData(
+                        name = "INPUT_FILE",
+                        value = if (useQuotes) "\"$currentLink\"" else currentLink
+                    )
+                )
+                it.add(
+                    InputParsedData(
+                        name = "INPUT_URL",
+                        value = if (useQuotes) "\"$currentLink\"" else currentLink
+                    )
+                )
+                it.add(InputParsedData(name = "HOST", value = if (useQuotes) "\"$host\"" else host))
+                it.add(
+                    InputParsedData(
+                        name = "FILENAME",
+                        value = if (useQuotes) "\"$filename\"" else filename
+                    )
+                )
                 it.add(InputParsedData(name = "RAND", value = (1000..9999).random().toString()))
             }
+
+            val quotedCommandExtraInputs = commandExtraInputs.map {
+                it
+            }
+
 
 
             Timber.d("Command to run: ${item.exec} ${resultCommand}")
 
 
-            val processResult = processManagerService.runCommandForShareWithEnv2(item, fullExecPath, resultCommand, path ,inputParsedData,commandExtraInputs,currentLink,processId, JobType.URL,usePython, isShellScript)
+            val processResult = processManagerService.runCommandForShareWithEnv2(
+                item,
+                fullExecPath,
+                resultCommand,
+                path,
+                inputParsedData,
+                if (execType == ExecType.SHELL_INSTALLED) quotedCommandExtraInputs else commandExtraInputs,
+                currentLink,
+                processId,
+                JobType.URL,
+                usePython,
+                isShellScript
+            )
 
             val result = processResult.toCommandResult(JobType.URL, currentLink)
 
