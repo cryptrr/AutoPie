c7a910015cc46b276eb9247b78efee999b8aac2a
amal <amalshaji645@gmail.com>
2026-06-27 04:27:23 +0530

    Added new-root arg for build_with_termux.sh

--- build_with_termux.sh
+++ build_with_termux.sh
@@ -7,28 +7,45 @@ export GRADLE_USER_HOME="${GRADLE_USER_HOME:-$ROOT_DIR/.gradle}"
 
 SKIP_PREPARE=false
 SKIP_BOOTSTRAP=false
+NEW_ROOT_DIR="${TERMUX_BOOTSTRAP_NEW_ROOT_DIR:-}"
 GRADLE_ARGS=()
 
-for arg in "$@"; do
-    case "$arg" in
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
         --skip-prepare)
             SKIP_PREPARE=true
+            shift
             ;;
         --skip-bootstrap)
             SKIP_BOOTSTRAP=true
+            shift
+            ;;
+        --new-root-dir|--new-root)
+            if [[ "$#" -lt 2 ]]; then
+                echo "Error: $1 requires a path argument." >&2
+                exit 1
+            fi
+            NEW_ROOT_DIR="$2"
+            shift 2
             ;;
         *)
-            GRADLE_ARGS+=("$arg")
+            GRADLE_ARGS+=("$1")
+            shift
             ;;
     esac
 done
 
+ROOT_ARGS=()
+if [[ -n "$NEW_ROOT_DIR" ]]; then
+    ROOT_ARGS=(--new-root-dir "$NEW_ROOT_DIR")
+fi
+
 if [[ "$SKIP_PREPARE" != true ]]; then
-    "$ROOT_DIR/scripts/prepare-termux-app.sh"
+    "$ROOT_DIR/scripts/prepare-termux-app.sh" "${ROOT_ARGS[@]}"
 fi
 
 if [[ "$SKIP_BOOTSTRAP" != true ]]; then
-    "$ROOT_DIR/scripts/prepare-termux-bootstrap.sh"
+    "$ROOT_DIR/scripts/prepare-termux-bootstrap.sh" "${ROOT_ARGS[@]}"
 fi
 
 if [[ "${#GRADLE_ARGS[@]}" -eq 0 ]]; then
