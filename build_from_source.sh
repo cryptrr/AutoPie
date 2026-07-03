c147df6c557b6d8afa4cd0c39eabe6a18dbfb907
amal <amalshaji645@gmail.com>
2026-07-03 12:46:28 +0530

    Updated pinned generator ref x14

--- build_from_source.sh
+++ build_from_source.sh
@@ -6,7 +6,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ASSETS_DIR="${AUTOPIE_ASSETS_DIR:-$ROOT_DIR/app/src/main/assets}"
 GENERATOR_REPOSITORY="${TERMUX_GENERATOR_REPOSITORY:-https://github.com/cryptrr/termux-generator.git}"
 # This revision contains the native, Docker-free bootstrap builder used here.
-GENERATOR_REF="${TERMUX_GENERATOR_REF:-fc59afcca0f981da6deb38cf176467a1b90ded87}"
+GENERATOR_REF="${TERMUX_GENERATOR_REF:-5341e4a5a37c6fe8c53ea1e52d565798ecc45c19}"
 
 WORK_DIR="$(mktemp -d "$ROOT_DIR/.termux-generator.XXXXXX")"
 GENERATOR_DIR="$WORK_DIR/termux-generator"
